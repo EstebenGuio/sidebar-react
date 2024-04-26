e47021625e1fc4186dfc50a2fdf2b5f208c77440
Esteben Guio <Esteben.guiog@gmail.com>
2024-04-25 21:33:58 -0500

    Feat: adding data-test-id attributes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 
 const App = () => {
+
+  //Create states for button
   const [isExpand, setIsExpand] = useState({})
+
+  //Create basic menu with subitems
   const menuConfig = [
     {
       title: 'Home'
@@ -16,11 +20,28 @@ const App = () => {
     }
   ]
 
+  /**
+   * Menu to render 
+   * 
+   * 1. Check if item has subItems
+   * 2. Creatae a button with its statuses
+   * 3. Create the event to expand or hide subitems on click
+   * 4. Create dinamically the text of the button based on the status
+   * 5. Print the subItems list if affly
+   * 
+   * 
+   * @param {*} menu 
+   * @returns 
+   */
   const renderObj = (menu) => {
+    //1
     if (menu.subItems) {
+
       return (
         <>
           <button
+          data-test-id={"button-"+menu.title.toLowerCase()}
+            //3
             onClick={() => {
               setIsExpand({
                 ...isExpand,
@@ -28,13 +49,22 @@ const App = () => {
               })
             }}
           >
-            {!isExpand?.[menu.title] ? 'Hide' : 'Expand'}
+            { //4
+              !isExpand?.[menu.title] ? 'Hide' : 'Expand'
+            }
           </button>
           {
+            //5
             !isExpand?.[menu.title] ? (
-              <ul key={menu.title}>
+              <ul
+              key={menu.title}
+              data-test-id={"ul-"+menu.title.toLowerCase()}
+              >
                 {menu.subItems.map((subitem) => {
-                  return <li key={subitem}>{subitem}</li>
+                  return <li 
+                  key={subitem}
+                  data-test-id={"li-"+menu.title.toLowerCase()}
+                  >{subitem}</li>
                 })}
               </ul>
             ) : null
